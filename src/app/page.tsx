f01055769ae38a484eafb3b4b50b48b5e6d42185
MH-Shawon <mohsinshawon18@gmail.com>
2025-02-03 01:24:16 +0600

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,30 +43,29 @@ export default function PricingPage() {
   ]
 
   return (
-    <div className="h-auto bg-[#CDFFFF] ">
+    <div className="h-auto bg-[#CDFFFF]">
       <CommonLayout>
+        <div className="mx-auto w-[100%] ">
+          <div className="text-center mb-8">
+            <h1 className="text-[48px] font-semibold mb-2 text-[#000] tracking-[-0.444px] leading-[64px]">
+              Pricing Plans For Professionals
+            </h1>
 
-      <div className="mx-auto w-[100%]">
-        <div className="text-center mb-8">
-          <h1 className="text-[48px] font-semibold mb-2 text-[#000] tracking-[-0.444px] leading-[64px]">
-            Pricing Plans For Professionals
-          </h1>
+            <p className="text-black mb-6 tracking-[-0.16px] text-center text-base font-normal leading-[23.111px] font-workSans">
+              Choose the best plan for your business. Change plans as you grow.
+            </p>
 
-          <p className="text-black mb-6 tracking-[-0.16px] text-center text-base font-normal leading-[23.111px] font-workSans">
-            Choose the best plan for your business. Change plans as you grow.
-          </p>
+            <select className="w-full md:w-[30%] rounded-md  bg-[#006] text-white px-2 py-2 text-sm">
+              <option>Choose your profession to see the pricing plans</option>
+            </select>
+          </div>
 
-          <select className="w-full md:w-[30%] rounded-md  bg-[#006] text-white px-2 py-2 text-sm">
-            <option>Choose your profession to see the pricing plans</option>
-          </select>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <PricingCard {...plans[0]} />
+            <PricingCard {...plans[1]} variant="featured" />
+            <PricingCard {...plans[2]} />
+          </div>
         </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          <PricingCard {...plans[0]} />
-          <PricingCard {...plans[1]} variant="featured" />
-          <PricingCard {...plans[2]} />
-        </div>
-      </div>
       </CommonLayout>
     </div>
   );
